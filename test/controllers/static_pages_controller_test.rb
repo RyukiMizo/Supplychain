37641f60c53cefcfcf815205c7d9b228601dca12
EC2 Default User <ec2-user@ip-172-31-28-189.us-east-2.compute.internal>
2018-08-12 05:23:46 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Blanding"
+    assert_select "title", "Branding"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Blanding"
+    assert_select "title", "Help | Branding"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Blanding"
+    assert_select "title", "About | Branding"
   end
   
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | Blanding"
+    assert_select "title", "Contact | Branding"
   end
 end
