ccf010ac811968c4615113211c65160848c1e158
EC2 Default User <ec2-user@ip-172-31-28-189.us-east-2.compute.internal>
2018-09-02 06:58:11 +0000

    edit

--- app/models/introduction.rb
+++ app/models/introduction.rb
@@ -1,6 +1,7 @@
 class Introduction < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :affilitation, length: {maximum: 255}
+  validates :affilitation, length: {maximum: 60}
   validates :content, length: {maximum: 1000}
+  validates :site, length: {maximum: 255}
 end
