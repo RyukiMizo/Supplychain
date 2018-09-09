dd2e3868f854df4a6b9e6a6c970df85d5767899f
EC2 Default User <ec2-user@ip-172-31-28-189.us-east-2.compute.internal>
2018-09-09 05:22:03 +0000

    modify chaincontroller

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -2,12 +2,19 @@ class ChainsController < ApplicationController
     before_action :logged_in_user, only: [:new]
     def new
         @chain = current_user.chains.build if logged_in?
+        @chain.deepchains.build if logged_in?
     end
     
     def create
         @chain = current_user.chains.build(chain_params)
         @chain.micropost_id = params[:id]
         @user = Micropost.find(params[:id]).user
+        @deepchains = @chain.deepchains
+        if @deepchains.nil?
+            flash[:danger] = "入力してください"
+            redirect_to "/postpage/#{params[:id]}"
+        end
+            
           if current_user.followers.include?(@user) || current_user == @user
               
             if @chain.save
@@ -16,7 +23,7 @@ class ChainsController < ApplicationController
             else
               render 'new'
             end 
-            
+
           else
               flash[:danger] = "フォローされていないため追加権限がありません"
               redirect_to user_url(@user)
