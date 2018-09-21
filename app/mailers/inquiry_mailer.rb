6dae9d89cc7d66143102c35a1aa3339dcb499b88
EC2 Default User <ec2-user@ip-172-31-28-189.us-east-2.compute.internal>
2018-09-21 09:08:49 +0000

    modify mailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
     def send_mail(inquiry)
         @inquiry = inquiry
           mail(
-          from: 'noreply.example.com',
+          from: 'noreply@example.com',
           to:   'branding9872@gmail.com',
           subject: 'お問い合わせ通知'
           )
